069d3019676e1c7d5851cc0c75f00d6251fc936b
Lionel Duboeuf <lionel.duboeuf@actility.com>
2019-11-06 10:11:30 +0100

    remove build folder

--- build/CMakeFiles/sm_FILES.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "sm_automoc.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sm_FILES.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
